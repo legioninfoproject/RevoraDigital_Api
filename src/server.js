693fba1cf0e34b6b66f533de6b3916a86bc641d2
JinoNadarajan <legioninfoproject@gmail.com>
2026-05-25 17:18:55 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,12 +11,25 @@ const serverIntiate = async () => {
   const app = express();
 
   // CORS connection for frontend
-  app.use(
-    cors({
-      origin: "http://localhost:3000",
-      credentials: true,
-    })
-  );
+   app.use((req, res, next) => {
+
+        res.header('Access-Control-Allow-Origin', "http://localhost:4200");
+        res.header('Access-Control-Allow-Headers', true);
+        res.header('Access-Control-Allow-Credentials', 'Content-Type');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      
+        res.header('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+        next();
+      });
+       const corsOptions = {
+        origin: '*',
+        credentials: true,            //access-control-allow-credentials:true
+        optionSuccessStatus: 200,
+    }
+    app.use(cors(corsOptions));
+    
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   // logger
