5162794ac4e3c555a8e73538427db4607f2b3d51
JinoNadarajan <legioninfoproject@gmail.com>
2026-05-26 10:25:07 +0530

    node-modules

--- src/server.js
+++ src/server.js
@@ -1,44 +1,33 @@
 const express = require("express");
 const cors = require("cors");
+const expressip = require("express-ip");
+const bodyparser = require("body-parser");
 const morgan = require("morgan");
 const config = require("./config/db");
 const dataConfig = require('./config/config');
 const appRoute = require("./appRoute");
 
-const serverIntiate = async () => {
-  await config.establishConnection();
+const startServer = async() =>{
+    await config.establishConnection();
+    const app = express();
 
-  const app = express();
-
-  // CORS connection for frontend
-   app.use((req, res, next) => {
-
-        res.header('Access-Control-Allow-Origin', "http://localhost:4200");
-        res.header('Access-Control-Allow-Headers', true);
-        res.header('Access-Control-Allow-Credentials', 'Content-Type');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-      
-        res.header('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-        next();
-      });
-       const corsOptions = {
-        origin: '*',
-        credentials: true,            //access-control-allow-credentials:true
+    const corsOptions = {
+        origin: "*", // Must be specific for cookies to work
+        credentials: true,
         optionSuccessStatus: 200,
-    }
+    };
     app.use(cors(corsOptions));
-    
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  // logger
-  app.use(morgan("dev"));
-  appRoute.Init(app);
-  // Create HTTP server
-  const http = require("http");
-  const server = http.createServer(app);
-   server.listen(dataConfig?.app?.port, '0.0.0.0', () => console.log(`Application listening on the port :${dataConfig.app.port}`));
-};
+    app.use(expressip().getIpInfoMiddleware);
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({ extended: true }));
+    app.use(morgan("dev"));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.listen(dataConfig?.app?.port, () => {
+        console.log(`Server is running on port ${dataConfig.app.port}`);
+    });
 
-serverIntiate();
+
+}
+
+startServer();
