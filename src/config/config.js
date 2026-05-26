767b4f0e3884a6f292925d110f3017cb038e1de9
JinoNadarajan <legioninfoproject@gmail.com>
2026-05-26 10:54:40 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ const config = {
   },
 
   dataBase: {
-    url:"mongodb+srv://legioninfoproject_db_user:Legion%402026@cluster0.8v2yext.mongodb.net/revora_db",
+    url:"mongodb+srv://legioninfoproject_db_user:Legion%402026@cluster0.8v2yext.mongodb.net/revora?retryWrites=true&w=majority"
   },
 };
 
